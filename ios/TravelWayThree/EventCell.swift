7154ae8d8de9589679b87f52574c65cf1b310311
Fredrik Nannestad <Fredrik@host-78-75-73-50.homerun.telia.com>
2015-10-02 18:19:01 +0200

    Created events

--- ios/TravelWayThree/EventCell.swift
+++ ios/TravelWayThree/EventCell.swift
@@ -8,19 +8,40 @@
 
 import UIKit
 
-class EventCell: UITableViewCell {
-
-  @IBOutlet weak var eventIcon: UIImageView!
-  @IBOutlet weak var card: Card!
-  @IBOutlet weak var eventTitle: UILabel!
-  @IBOutlet weak var eventText: UILabel!
-  @IBAction func didPressJoin(sender: UIButton) {
+class EventIconView: UIView {
     
-  }
-  
-  
+    @IBOutlet weak var icon: UIImageView!
+    
+    var type: EventType = .Food {
+        didSet {
+            self.backgroundColor = type.color
+            
+        }
+    }
+    
+    override func awakeFromNib() {
+        
+    }
+    
+}
+
+class EventCell: UITableViewCell {
+    
+    @IBOutlet weak var eventIcon: UIImageView!
+    @IBOutlet weak var card: Card!
+    @IBOutlet weak var eventTitle: UILabel!
+    @IBOutlet weak var eventText: UILabel!
+    @IBAction func didPressJoin(sender: UIButton) {
+        
+    }
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func configureWithEvent(event: Event) {
+        card.backgroundColor = event.color
+        
     }
 }
